0faa1309a1816f330b7b3c9a25fecc7bc915ec8d
VoT1 <k.kapinski@bioplanet.pl>
2023-04-21 15:17:56 +0200

    update 0.21

--- app/src/main/java/com/example/przepyszneapp2/DatabaseHelper.java
+++ app/src/main/java/com/example/przepyszneapp2/DatabaseHelper.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "dbpyszneapp2.db";
-    private static final int DATABASE_VERSION = 6;
+    private static final int DATABASE_VERSION = 8;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,9 +20,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS Uzytkownicy (id INTEGER PRIMARY KEY, nazwa TEXT, haslo TEXT, admin BOOLEAN)");
         db.execSQL("CREATE TABLE IF NOT EXISTS Przepisy (id INTEGER PRIMARY KEY, nazwa TEXT, produkt1 TEXT, produkt2 TEXT, produkt3 TEXT)");
-        db.execSQL("CREATE TABLE IF NOT EXISTS Produkty (id INTEGER PRIMARY KEY, nazwa TEXT, waga TEXT)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS Produkty (id INTEGER PRIMARY KEY, nazwa TEXT, waga TEXT,kcal TEXT )");
         db.execSQL("INSERT INTO Uzytkownicy (id, nazwa, haslo, admin) VALUES (1, 'admin', '1234', 1)");
         db.execSQL("INSERT INTO Uzytkownicy (id, nazwa, haslo, admin) VALUES (2, 'user', '1234', 0)");
+        db.execSQL("INSERT INTO Produkty (id, nazwa, waga, kcal) VALUES (1, 'pomidor', '40g', '46kcal')");
+        db.execSQL("INSERT INTO Produkty (id, nazwa, waga, kcal) VALUES (2, 'ogorek', '30g', '36kcal')");
         db.execSQL("INSERT INTO Przepisy (id,nazwa,produkt1,produkt2,produkt3)VALUES (1,'test1','test','test','test')");
         db.execSQL("INSERT INTO Przepisy (id,nazwa,produkt1,produkt2,produkt3)VALUES (2,'test2','test','test','test')");
     }
@@ -31,6 +33,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS Uzytkownicy");
         db.execSQL("DROP TABLE IF EXISTS Przepisy");
+        db.execSQL("DROP TABLE IF EXISTS Produkty");
         onCreate(db);
     }
 
--- app/src/main/java/com/example/przepyszneapp2/MainActivity2.java
+++ app/src/main/java/com/example/przepyszneapp2/MainActivity2.java
@@ -3,12 +3,15 @@ package com.example.przepyszneapp2;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,15 +22,17 @@ import java.util.ArrayList;
 public class MainActivity2 extends AppCompatActivity {
 
     private DatabaseHelper dbHelper;
-    private ListView listview;
     private Button buttonwyszukaj;
     private boolean isBackPressed = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         buttonwyszukaj = findViewById(R.id.buttonwyszukaj);
+        ListView listViewfiltr = findViewById(R.id.listviewfiltr);
+        ListView listView = findViewById(R.id.listview);
 
 
         buttonwyszukaj.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +40,7 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View v) {
                 dbHelper = new DatabaseHelper(MainActivity2.this);
                 SQLiteDatabase db = dbHelper.getReadableDatabase();
+                listView.setVisibility(View.GONE);
 
                 Cursor cursorprodukty = db.rawQuery("SELECT * FROM Produkty", null);
 
@@ -43,12 +49,11 @@ public class MainActivity2 extends AppCompatActivity {
                     String nazwa = cursorprodukty.getString(cursorprodukty.getColumnIndexOrThrow("nazwa"));
                     String waga = cursorprodukty.getString(cursorprodukty.getColumnIndexOrThrow("waga"));
                     String item = nazwa + ", " + waga;
+
                     itemsprodukty.add(item);
                 }
 
                 cursorprodukty.close();
-                db.close();
-
                 final String[] produktyArray = itemsprodukty.toArray(new String[0]);
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this, R.style.StylDialoguProdukty);
@@ -59,21 +64,48 @@ public class MainActivity2 extends AppCompatActivity {
                 builder.setMultiChoiceItems(produktyArray, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                        // TODO: obsługa zaznaczania elementów w menu
+                        if (isChecked) {
+                            // Ustawienie tła dla wybranego elementu
+                            View view = ((AlertDialog) dialog).getListView().getChildAt(which);
+                            if (view != null) {
+                                view.setBackgroundColor(Color.parseColor("#FFFFE0"));
+                            }
+                        } else {
+                            // Usunięcie tła dla odznaczonego elementu
+                            View view = ((AlertDialog) dialog).getListView().getChildAt(which);
+                            if (view != null) {
+                                view.setBackgroundResource(android.R.color.transparent);
+                            }
+                        }
                     }
                 });
 
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // TODO: obsługa zatwierdzania wyboru
-                    }
-                });
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                ArrayList<String> items = new ArrayList<>();
+                                Cursor cursor = db.rawQuery("SELECT * FROM Przepisy ", null);
+                                while (cursor.moveToNext()) {
+                                    String id = cursor.getString(cursor.getColumnIndexOrThrow("id"));
+                                    String nazwa = cursor.getString(cursor.getColumnIndexOrThrow("nazwa"));
+                                    String produkt1 = cursor.getString(cursor.getColumnIndexOrThrow("produkt1"));
+                                    String produkt2 = cursor.getString(cursor.getColumnIndexOrThrow("produkt2"));
+                                    String produkt3 = cursor.getString(cursor.getColumnIndexOrThrow("produkt3"));
+                                    String item = ": " + id + ", " + nazwa + ", " + produkt1 + ", " + produkt2 + ", " + produkt3;
+                                    items.add(item);
+                                }
+                                cursor.close();
+                                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity2.this, android.R.layout.simple_list_item_1, items);
+                                listViewfiltr.setAdapter(adapter);
+                            }
+                        });
 
                 builder.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // TODO: obsługa anulowania wyboru
+                        Toast.makeText(MainActivity2.this, "Wybor anulowany", Toast.LENGTH_SHORT).show();
+                        listView.setVisibility(View.VISIBLE);
                     }
                 });
 
@@ -96,9 +128,6 @@ public class MainActivity2 extends AppCompatActivity {
         }
 
         cursorprodukty.close();
-
-
-        ListView listView = findViewById(R.id.listview);
         ArrayList<String> items = new ArrayList<>();
         Cursor cursor = db.rawQuery("SELECT * FROM Przepisy", null);
        while (cursor.moveToNext()) {
@@ -116,6 +145,8 @@ public class MainActivity2 extends AppCompatActivity {
         db.close();
 
     }
+
+
 
 
     @Override
